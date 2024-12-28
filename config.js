fbd023777d3d892c8baf8fa755dfb1c6de6d70f7
Prasannakumara <153364276+Prasannakumara@users.noreply.github.com>
2024-12-28 16:22:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=jc0RTYCK#oTkYtC0VqlXIl-puMpnN61nQRwAmqgrJnXnnCfU7Gp4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=uJUVHJIL#5xrHGNKPwyIB9zJJ0Iqzt6Azn5ryMsIHzOV2xaaKnhE' : process.env.SESSION_ID,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Put your github username': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
 };
